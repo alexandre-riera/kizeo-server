8425f41a9e436477bbd02d63c59605c8e2e0197f
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-08-27 18:37:52 +0200

    Debug image de fond de pdf client

--- src/Controller/EquipementPdfController.php
+++ src/Controller/EquipementPdfController.php
@@ -1477,7 +1477,7 @@ class EquipementPdfController extends AbstractController
     private function getImageUrlForAgency(string $agencyName): string
     {
         // Assurer que cela renvoie un chemin absolu
-        $basePath = '%kernel.project_dir%/public/img/background/';
+        $basePath = '/img/background/';
 
         // Assurez-vous d'ajouter vos conditions pour les URL spécifiques
         switch ($agencyName) {
