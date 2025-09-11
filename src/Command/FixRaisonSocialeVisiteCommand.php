3a73e668100d2e9905e1c22a2af30abf322479f0
alexandre-riera <alexandre.riera.dev@gmail.com>
2025-09-11 20:05:57 +0200

    FIX traitement de tous les formulaires au 11 septembre 2025 14h28

--- src/Command/FixRaisonSocialeVisiteCommand.php
+++ src/Command/FixRaisonSocialeVisiteCommand.php
@@ -72,7 +72,7 @@ class FixRaisonSocialeVisiteCommand extends Command
                     $codeEquipement = $form->getCodeEquipement();
                     
                     // Trouver l'équipement correspondant
-                    $equipment = $repository->findOneBy(['numero_equipement ' => $codeEquipement]);
+                    $equipment = $repository->findOneBy(['numeroEquipement ' => $codeEquipement]);
                     
                     if ($equipment) {
                         $raisonSocialeVisite = $equipment->getRaisonSociale() . "\\" . $equipment->getVisite();
