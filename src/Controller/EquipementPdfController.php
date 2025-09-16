f9b71613e08b8b811f883acdbdae72e21079a992
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-09-16 17:30:44 +0200

    Update tables equipements en front

--- src/Controller/EquipementPdfController.php
+++ src/Controller/EquipementPdfController.php
@@ -251,7 +251,7 @@ class EquipementPdfController extends AbstractController
             $uniqueEquipments = [];
             $duplicatesRemoved = 0;
             foreach ($allEquipmentsWithPictures as $equipmentData) {
-                $numeroEquipement = $equipmentData->getNumeroEquipement();
+                $numeroEquipement = $equipmentData['numero_equipement'];
                 $equipment = $equipmentData['equipment'];
                 
                 try {
