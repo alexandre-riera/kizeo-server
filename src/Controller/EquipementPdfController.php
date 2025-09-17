36a26ee970f24d98369eb305e686b44344af4f26
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-09-17 08:10:08 +0200

    Update tables equipements en front

--- src/Controller/EquipementPdfController.php
+++ src/Controller/EquipementPdfController.php
@@ -311,7 +311,7 @@ class EquipementPdfController extends AbstractController
                 'code_equipement' => $equipment->getNumeroEquipement(),
                 'raison_sociale_visite' => $equipment->getRaisonSociale() . '\\' . $equipment->getVisite()
             ]);
-            dump($equipment);
+            dump($equipment->getMiseEnService());
             $formattedEquipements[] = [
                 'numero' => $equipment->getNumeroEquipement() ?? 'N/A',
                 'type' => ['libelle' => $equipment->getLibelleEquipement() ?? 'Non renseigné'],
