6fdbf6ab8cb9bddfb828850ba2cd134c1958e50d
Riera alexandre <alexandre.riera.simplon@gmail.com>
2025-09-17 07:57:42 +0200

    Update tables equipements en front

--- src/Controller/EquipementPdfController.php
+++ src/Controller/EquipementPdfController.php
@@ -322,8 +322,8 @@ class EquipementPdfController extends AbstractController
                 'repereSite' => $equipment->getRepereSiteClient() ?? '',
                 'anomalies' => $this->getEquipmentAnomalies($equipment),
                 // 'photosPrincipales' => array_slice($photos, 0, 1), // Première photo
-                'photosPrincipales' => $photos[0]['photo_2'],
-                'photosSecondaires' =>  $photos[0]['photo_compte_rendu'],   // Autres photos
+                'photosSecondaires' =>  $photos[0]->getPhoto2(),
+                'photosPrincipales' => $photos[0]->getPhotoCompteRendu()
             ];
         }
 
